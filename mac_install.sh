abaced2e10a9885688f8edd9cbc5ce2995f4bba4
Norbert Bartko <bartko.norbert@gmail.com>
2022-11-28 12:28:42 +0100

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+# Configure bash
+/bin/bash -c "$(curl -o ~/.bashrc https://raw.githubusercontent.com/NBprojekt/Dotfiles/HEAD/.bashrc)"
+/bin/bash -c "$(curl -o ~/.bash_colors https://raw.githubusercontent.com/NBprojekt/Dotfiles/HEAD/.bash_colors)"
+/bin/bash -c "$(curl -o ~/.bash_aliases https://raw.githubusercontent.com/NBprojekt/Dotfiles/HEAD/.bash_aliases)"
+source ~/.bashrc
+
+# Install homebrew if not available
+if ( ! command -v brew &> /dev/null ); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 # Enable fingerprint sudo
 # Add `auth sufficient pam_tid.so` to `/etc/pam.d/sudo`
