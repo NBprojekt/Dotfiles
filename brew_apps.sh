efa7d6e8ab103d165e97839db80b7b8094cc4988
Norbert Bartko <bartko.norbert@gmail.com>
2025-01-20 11:55:49 +0100

    Create brew_apps.sh

---
+++ brew_apps.sh
@@ -0,0 +1,17 @@
+brew install -y coreutils
+brew install -y binutils
+brew install -y diffutils
+brew install -y ed --with-default-names
+brew install -y findutils --with-default-names
+brew install -y gawk
+brew install -y gnu-indent --with-default-names
+brew install -y gnu-sed --with-default-names
+brew install -y gnu-tar --with-default-names
+brew install -y gnu-which --with-default-names
+brew install -y gnutls
+brew install -y grep --with-default-names
+brew install -y gzip
+brew install -y screen
+brew install -y watch
+brew install -y wdiff --with-gettext
+brew install -y wget
